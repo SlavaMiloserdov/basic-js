87cff683aa090972d7cff0877c17b7c1fb053530
SlavaMiloserdov <slavmiloserdov@mail.ru>
2020-02-26 20:02:33 +0400

    feat: add solution for transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,52 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) throw new Error();
+  return transformArr(transformArr(arr));
 };
+
+function transformArr(arr) {
+  let result = [];
+  for (let index = 0; index < arr.length; index++) {
+    if (!String(arr[index]).match(/--/)) {
+      let { newArr, flag } = getNewArr(index, arr);
+      if (flag < 0) {
+        arr[index - 1] !== undefined
+          ? result.push("--discard-prev")
+          : result.push("--discard-next");
+      } else {
+        newArr.forEach(i => {
+          i === undefined ? result.push(NaN) : result.push(i);
+        });
+      }
+    }
+  }
+  return result;
+}
+
+function getNewArr(index, array) {
+  let res = [];
+  let counter = 1;
+  let discardNext = "--discard-next";
+  let discardPrev = "--discard-prev";
+  let doubleNext = "--double-next";
+  let doublePrev = "--double-prev";
+  switch (array[index - 1]) {
+    case doubleNext:
+      counter++;
+      break;
+    case discardNext:
+      counter--;
+      break;
+  }
+  switch (array[index + 1]) {
+    case doublePrev:
+      counter++;
+      break;
+    case discardPrev:
+      counter--;
+      break;
+  }
+  for (let i = 0; i < counter; i++) {
+    res.push(array[index]);
+  }
+  return { newArr: res, flag: counter };
+}
